002e9dbd4217fa7e3b7ed8268a5025641e54c014
Thang Le Quang <lethang7794@gmail.com>
2021-01-18 17:10:39 +0700

    Reformat loading spinner in Top Rated page

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -56,36 +56,38 @@ const TopRated = () => {
             <h4>Top Rated</h4>
                 <p>Discover the highest rated ever.</p>
             
-            <Slider {...settings}>
+            {loading? (
+              <div className="ratio ratio-16x9 d-flex justify-content-center align-items-center">
+                <Spinner animation="border" role="status" style={{position: 'static'}}>      
+                  <span className="sr-only">Loading...</span>
+                </Spinner>
+              </div>        
+   
+            ) : (
+              <Slider {...settings}>
+                {movies.map((movie) => (
+                  <Card
+                      // style={{ width: 25 % visualViewport, bg: "dark" }}
+                      mr-auto>
+
+                      <Card.Body>
+                          <Card.Img className="pb-2"
+                                  // style={{ width: '25%vw' }}
+                                  src={`http://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="slide" />
+                          <p><strong>{movie.title}</strong></p>
+                          <p className="py-0"><small>
+                              <strong>Rating: </strong>{movie.vote_average} | 
+                              <strong> Released on: </strong>{movie.release_date.match(/\d+/)}<br/>
+                              {/* <strong>Storyline: </strong>{movie.overview} */}
+                          </small></p>
+                          <Button variant="outline-dark" size="sm" href={`http://localhost:3000/movie/${movie.id}`}>Watch</Button>
+
+                      </Card.Body>
+                  </Card>))}
+              </Slider> 
+            )}       
                 
-            {loading? 
-            <Spinner animation="border" role="status">      
-            <span className="sr-only">Loading...</span>
-            </Spinner>           
-
-                    
-            :movies.map((movie) => (
-                <Card
-                    // style={{ width: 25 % visualViewport, bg: "dark" }}
-                    mr-auto>
-
-                    <Card.Body>
-                        <Card.Img className="pb-2"
-                                // style={{ width: '25%vw' }}
-                                src={`http://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="slide" />
-                        <p><strong>{movie.title}</strong></p>
-                        <p className="py-0"><small>
-                            <strong>Rating: </strong>{movie.vote_average} | 
-                            <strong> Released on: </strong>{movie.release_date.match(/\d+/)}<br/>
-                            {/* <strong>Storyline: </strong>{movie.overview} */}
-                        </small></p>
-                        <Button variant="outline-dark" size="sm" href={`http://localhost:3000/movie/${movie.id}`}>Watch</Button>
-
-                    </Card.Body>
-                </Card>
-            ))}       
-                
-                </Slider>  
+                 
                 <div class="mx-auto" style={{ height:"50px" }}></div>
             </Container>                    
         </div>
