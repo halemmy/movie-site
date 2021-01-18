0e34eea4c31b5c9f98abd0034c9810f64448a03a
Thang Le Quang <lethang7794@gmail.com>
2021-01-18 17:29:00 +0700

    Reformat NavBar
    - Prevent unloaded logo from breaking navbar layout.
    - Use hamburger button in NavBar to prevent NavItems show up in 3 lines for mobile devices.

--- src/components/PublicNavBar.js
+++ src/components/PublicNavBar.js
@@ -11,15 +11,18 @@ const PublicNavBar = () => {
     return (
     <Navbar bg="light" expand="lg" justify-content-betweend>
       <Navbar.Brand>
-        <img src={logo} alt="Apple TV" width="50px" />
+        <img src={logo} alt="Apple TV" width="50px" height="50px"/>
       </Navbar.Brand>
-      <Nav className="mr-auto">
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
           <Nav.Link as={Link} to="/">WatchNow</Nav.Link>
           <Nav.Link as={Link} to="/upcoming">Upcoming</Nav.Link>
           <Nav.Link as={Link} to="/top_rated">Top Rated</Nav.Link>
-
-
         </Nav>
+      </Navbar.Collapse>      
       {/* <SearchBar /> */}
     </Navbar>
   );
